0378121f4c7b4353a685231c70d222ad8ab1d6ab
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-05 15:31:27 -0400

    Assignment completed

--- basketball_dictionaries.py
+++ basketball_dictionaries.py
@@ -5,6 +5,13 @@ class Player:
         self.position = dict["position"]
         self.team = dict["team"]
 
+    @classmethod
+    def get_team(cls, team_list):
+        new_list = []
+        for dict in team_list:
+            new_list.append(Player(dict))
+        return new_list
+
 
 kevin = {
     "name": "Kevin Durant", 
@@ -69,3 +76,13 @@ players = [
     }
 ]
 
+# new_team = []
+
+# for dict in players:
+#     new_team.append(Player(dict))
+
+# print(new_team[0].age)
+
+team1 = Player.get_team(players)
+for player in team1:
+    print(player.name)
